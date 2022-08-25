b688d0122bc6d95cabc375d45af04884e72c2872
Justin Toft <justintoft10@gmail.com>
2022-08-25 14:43:52 -0400

    Use object instead of passing each value to component

--- src/Note.js
+++ src/Note.js
@@ -7,17 +7,17 @@ class Note extends React.Component {
         return (
             <div className="note">
                 <div className="note-text-box-div">
-                    <textarea id="monsterName" className="note-monster-title" maxLength="12" defaultValue={this.props.monsterName}></textarea>
-                    <input type="number" id="monsterCurrentHealth" className="note-health"></input>
+                    <textarea id="monsterName" className="note-monster-title" maxLength="12" defaultValue={this.props.monsterInfo.monsterName}></textarea>
+                    <input type="number" id="monsterCurrentHealth" className="note-health" defaultValue={this.props.monsterInfo.curHealth}></input>
                     <textarea className="note-slash" readOnly="" tabIndex="-1" defaultValue="/"></textarea>
-                    <input type="number" id="monsterMaxHealth" className="note-health"></input>
+                    <input type="number" id="monsterMaxHealth" className="note-health" defaultValue={this.props.monsterInfo.maxHealth}></input>
                 </div>
                 <div className="note-info-bar">
                     <button className="note-heal">HEAL</button>
                     <input type="number" className="note-health-points" maxLength="3" placeholder="amount"></input>
                     <button className="note-damage">DMG</button>
                 </div>
-                <textarea className="note-general-text" id="monsterGeneralNotes"></textarea>
+                <textarea className="note-general-text" id="monsterGeneralNotes" defaultValue={this.props.monsterInfo.notes}></textarea>
                 <div className="note-trash-wrapper">
                     <button className="note-delete-icon">
                         <img src="/trash.svg" alt="TRASH"></img>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useState} from 'react';
+// import {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import AddNoteButton from './AddNoteButton';
@@ -8,17 +8,13 @@ import Note from './Note.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const data = [{ monsterName: 'Demogorgon'}, { monsterName: 'Mindflayer' }];
-
-// const handleClick() {
-//   setData(current => [...current, 'Carl']);
-// }
+const data = [{ monsterName: 'Demogorgon', maxHealth: 300, curHealth: 175, notes: "Stranger Things!"}, { monsterName: 'Mindflayer' }];
 
 root.render(
   <React.StrictMode>
     <div id="app">
       <AddNoteButton />
-      {data.map(note => <Note key={note.monsterName + "=" + Math.random() * 10000} monsterName={note.monsterName}/>)}
+      {data.map(note => <Note key={note.monsterName + "=" + Math.random() * 10000} monsterInfo={note}/>)}
     </div>
   </React.StrictMode>
 );
